397f940d2f92cda7f95bb3ba4b00e37021e314ee
avillarroyao <165774726+avillarroyao@users.noreply.github.com>
2024-09-30 12:25:50 +0200

    a

--- src/main/java/cat/tecnocampus/notes2324/application/dtos/NoteCreate.java
+++ src/main/java/cat/tecnocampus/notes2324/application/dtos/NoteCreate.java
@@ -9,7 +9,9 @@ import java.util.List;
 public record NoteCreate(
         long noteId,
         @Size(min=5, max=100)
-        @Pattern(regexp = "^[A-ZÀÁÈÉÍÒÓÚÜ][A-Za-zÀ-ÿ\s.,;:_'\\-]*", message = "Title must begin with a capital letter. Also only letters are allowed")
+        @Pattern(regexp = "^[A-Z].*", message ="Title must begin with a capital letter. Also only letters are allowed")
+
+        // @Pattern(regexp = "^[A-ZÀÁÈÉÍÒÓÚÜ][A-Za-zÀ-ÿ\s.,;:_'\\-]*", message = "Title must begin with a capital letter. Also only letters are allowed")
         String title,
         @Size(min=5, max=100)
         @Pattern(regexp = "^[A-ZÀÁÈÉÍÒÓÚÜ0-9].*", message = "Title must begin with a capital letter.")
